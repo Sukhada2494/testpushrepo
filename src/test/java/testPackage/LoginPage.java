ff4284730b12f0f507aac0e8957f855bc62afd85
Sukhada2494 <jirolesukhada@gmail.com>
2024-06-13 23:17:00 +0530

    Added second class

---
+++ src/test/java/testPackage/LoginPage.java
@@ -0,0 +1,10 @@
+package testPackage;
+
+public class LoginPage 
+{
+   public static void main(String[] args)
+   {
+	   System.out.println("Enter the username");
+	   System.out.println("Enter the password");
+   }
+}
